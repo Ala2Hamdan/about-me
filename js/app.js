c920ea5f907995365cb3c185c70d7728a37c4112
Ala2Hamdan <ala2hamdan92@gmail.com>
2021-02-08 18:48:13 +0200

    add js code and css style

---
+++ js/app.js
@@ -0,0 +1,63 @@
+'use strict';
+  let userName = prompt('what is yuor name ??');
+       console.log(userName);
+  alert(` wellcome ${userName} to my web site let us  start  a game !!!!`);
+    
+  
+
+  let favColor =prompt('Is my favorite color black??');
+    favColor= favColor.toLowerCase();
+
+  if (favColor==='yes'|| favColor==='y'){
+         console.log('that is right ' +favColor)
+      alert('that is right');
+  } else{
+         //console.log('wrong ' + favColor)
+      alert('wrong try to guess again');
+  }
+  
+  let favSub =prompt('Is my favorite subject biology??');
+    favSub= favSub.toLowerCase();
+
+  if (favSub==='no'|| favSub==='n'){
+           //console.log('that is right ' + favSub)
+      alert('that is right');
+  } else{
+           //console.log('wrong '+ favSub)
+      alert('wrong try to guess again');
+  }
+
+  let favD =prompt('Is my favorite drink juice??');
+  favD= favD.toLowerCase();
+
+  if (favD==='no'|| favD==='n'){
+        // console.log('that is right ' + favD);
+    alert('that is right');
+ } else{
+       // console.log('wrong ' + favD)
+    alert('wrong try to guess again');
+
+  }
+
+ let favGame =prompt('Is my favorite game Clash Royale??');
+    favGame= favGame.toLowerCase();
+
+  if (favGame==='no'|| favColor==='n'){
+         // console.log('that is right ' + favGame );
+      alert('that is right');
+  } else{
+       // console.log('wrong '+ favGame);
+      alert('wrong try to guess again');
+  }
+
+
+  let favFruit =prompt('Is my favorite fruit green apple ??');
+  favFruit= favFruit.toLowerCase();
+
+if (favFruit==='yes'|| favFruit==='y'){
+    // console.log('that is right ' + favFruit );
+    alert('that is right');
+} else{
+   // console.log('wrong '+ favFruit)
+    alert('wrong try to guess again');
+}
