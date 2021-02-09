4baf6e6424b85462bd1ff9f257fd46fa7f648651
Ala2Hamdan <ala2hamdan92@gmail.com>
2021-02-09 23:00:53 +0200

    add more if statement and loop

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# about-me-
+# about-me
+* this website about me,  but not all information is true excpet introduction.
+* I am building this website for assignment purpose .
+* this website contain :
+- HTML file its name `index.html` 
+       * image
+       * heading  `<h1>` 
+       * Unordered lists
+       * Ordered lists
+       
+
+-  javascript file its name `app.js` 
+       * prompt ask user his name .
+       * if else statement for user to guess right answer
+       * for loop for user to guess right answer  
+       * use "break" to stop loop. 
+
+- CSS file its name `style.css`
+
+     
--- js/app.js
+++ js/app.js
@@ -3,14 +3,15 @@
        console.log(userName);
   alert(` wellcome ${userName} to my web site let us  start  a game !!!!`);
     
-  
+  let score=0;
 
   let favColor =prompt('Is my favorite color black??');
     favColor= favColor.toLowerCase();
 
   if (favColor==='yes'|| favColor==='y'){
-         console.log('that is right ' +favColor)
+         //console.log('that is right ' +favColor)
       alert('that is right');
+      score=score+1;
   } else if(favColor==='no'|| favColor==='n'){
          //console.log('wrong ' + favColor)
       alert('wrong try to guess again');
@@ -25,6 +26,7 @@
   if (favSub==='no'|| favSub==='n'){
            //console.log('that is right ' + favSub)
       alert('that is right');
+      score=score+1;
   } else if (favSub==='yes'||favSub==='y'){
            //console.log('wrong '+ favSub)
       alert('wrong try to guess again');
@@ -40,6 +42,7 @@
   if (favD==='no'|| favD==='n'){
         // console.log('that is right ' + favD);
     alert('that is right');
+    score=score+1;
  } else if (favD==='yes' || favD=== 'y'){
        // console.log('wrong ' + favD)
     alert('wrong try to guess again');
@@ -56,6 +59,7 @@
   if (favGame==='no'|| favColor==='n'){
          // console.log('that is right ' + favGame );
       alert('that is right');
+      score=score+1;
   } else if (favGame === 'yes'|| favGame==='y'){
        // console.log('wrong '+ favGame);
       alert('wrong try to guess again');
@@ -71,6 +75,7 @@
 if (favFruit==='yes'|| favFruit==='y'){
     // console.log('that is right ' + favFruit );
     alert('that is right');
+    score=score+1;
 } else if (favFruit === 'no' || favFruit=='n') {
    // console.log('wrong '+ favFruit)
     alert('wrong try to guess again');
@@ -80,3 +85,48 @@ if (favFruit==='yes'|| favFruit==='y'){
  }
 
  alert('I hope you injoy with this guessing game, thank you ' +  userName);
+
+ 
+ 
+    for (let i=0; i<4 ; i++){
+      let age = prompt('guess my age ,I will help you , you have 4  attempts ');
+
+      if (parseInt(age)===29){
+       // console.log('true');
+        alert('that is correct');
+        score=score+1;
+        break;
+      }
+        else if (parseInt(age)<29){
+          //console.log('false');
+          alert('too low');
+        }
+        else  if (parseInt(age)> 29){
+          //console.log('false');
+          alert('too hight'); 
+        }
+        else 
+      {
+        alert('try again');
+      }
+     
+    }alert(' my age is 29');
+
+   let arrayA=['action','comedy','adventure','fition'];
+   let check = false;
+   for (let x=0; x<6; x++){
+     let userg = prompt('guess what kind of movies I like ??')
+     for (let j=0; j<arrayA.length;j++){
+       if (userg===arrayA[j]){
+         //console.log('true');
+         alert('that is correct');
+         check=true;
+         score=score+1;
+         break;  
+       }
+     }
+     if (check == true){break;}
+   }
+
+  // console.log( 'your score is : ' + score);
+   alert(`you get ${score} from 7 `);
