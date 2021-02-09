d232affc723cd3ae30a429aeeb9e34269551b3dd
Ala2Hamdan <ala2hamdan92@gmail.com>
2021-02-09 14:27:04 +0200

    modified

--- js/app.js
+++ js/app.js
@@ -11,10 +11,13 @@
   if (favColor==='yes'|| favColor==='y'){
          console.log('that is right ' +favColor)
       alert('that is right');
-  } else{
+  } else if(favColor==='no'|| favColor==='n'){
          //console.log('wrong ' + favColor)
       alert('wrong try to guess again');
   }
+     else{
+       alert('enter yes or y or no or n');
+     }
   
   let favSub =prompt('Is my favorite subject biology??');
     favSub= favSub.toLowerCase();
@@ -22,9 +25,13 @@
   if (favSub==='no'|| favSub==='n'){
            //console.log('that is right ' + favSub)
       alert('that is right');
-  } else{
+  } else if (favSub==='yes'||favSub==='y'){
            //console.log('wrong '+ favSub)
       alert('wrong try to guess again');
+  }
+  
+  else{
+    alert('enter yes or y or no or n');
   }
 
   let favD =prompt('Is my favorite drink juice??');
@@ -33,10 +40,14 @@
   if (favD==='no'|| favD==='n'){
         // console.log('that is right ' + favD);
     alert('that is right');
- } else{
+ } else if (favD==='yes' || favD=== 'y'){
        // console.log('wrong ' + favD)
     alert('wrong try to guess again');
 
+  }
+  
+  else{
+    alert('enter yes or y or no or n');
   }
 
  let favGame =prompt('Is my favorite game Clash Royale??');
@@ -45,9 +56,12 @@
   if (favGame==='no'|| favColor==='n'){
          // console.log('that is right ' + favGame );
       alert('that is right');
-  } else{
+  } else if (favGame === 'yes'|| favGame==='y'){
        // console.log('wrong '+ favGame);
       alert('wrong try to guess again');
+  }
+  else{
+    alert('enter yes or y or no or n');
   }
 
 
@@ -57,7 +71,12 @@
 if (favFruit==='yes'|| favFruit==='y'){
     // console.log('that is right ' + favFruit );
     alert('that is right');
-} else{
+} else if (favFruit === 'no' || favFruit=='n') {
    // console.log('wrong '+ favFruit)
     alert('wrong try to guess again');
-}
+ }
+  else{
+  alert('enter yes or y or no or n');
+ }
+
+ alert('I hope you injoy with this guessing game, thank you ' +  userName);
